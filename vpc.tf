297a10b474a057e000785d439de9642492025f38
opas95 <etolorcallister@gmail.com>
2023-06-15 10:06:58 +0300

    VPC set up

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "food-pub-sn" {
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "food-pub-sn"
+    Name = "food public sn"
   }
 }
 
@@ -24,10 +24,56 @@ resource "aws_subnet" "food-pub-sn" {
 resource "aws_subnet" "food-pvt-sn" {
   vpc_id     = aws_vpc.food-vpc.id
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "food-pvt-sn"
+    Name = "food prviate sn"
   }
 }
 
+#Internet gateway
+resource "aws_internet_gateway" "food-igw" {
+  vpc_id = aws_vpc.food-vpc.id
+
+  tags = {
+    Name = "food igw"
+  }
+}
+
+# PUBLIC ROUTE TABLE
+resource "aws_route_table" "food-pub-RT" {
+  vpc_id = aws_vpc.food-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.food-igw.id
+  }
+
+  tags = {
+    Name = "food public RT"
+  }
+}
+
+
+# PRIVATE ROUTE TABLE
+resource "aws_route_table" "food-pvt-RT" {
+  vpc_id = aws_vpc.food-vpc.id
+
+
+  tags = {
+    Name = "food public RT"
+  }
+}
+
+# SUBNET ROUTE TABLE ASSOCIATION - PUBLIC 
+resource "aws_route_table_association" "food-pub-asc" {
+  subnet_id      = aws_subnet.food-pub-sn.id
+  route_table_id = aws_route_table.food-pub-RT.id
+}
+
+# SUBNET ROUTE TABLE ASSOCIATION - PRIVATE RT
+resource "aws_route_table_association" "food-pvt-asc" {
+  subnet_id      = aws_subnet.food-pvt-sn.id
+  route_table_id = aws_route_table.food-pvt-RT.id
+}
 
