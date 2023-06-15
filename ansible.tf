688f681294fe98cbd59a98ddd18eefb2c6e12610
opas95 <etolorcallister@gmail.com>
2023-06-15 11:55:38 +0300

    key

--- ansible.tf
+++ ansible.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ansible-master" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.food-pub-sn.id
-  key_name = greenkey
+  key_name = "greenkey"
   vpc_security_groups_ids = [aws_security_group.allow_food.id]
   user_data = file("install-ansible")
   private_ip = "172.31.255.255"
